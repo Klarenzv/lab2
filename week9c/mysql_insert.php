1399f9f56ece89e0ab3660004eb1637cb41fb6b7
Klarenz <kavillanueva@Studetn.apc.edu.ph>
2023-02-10 15:15:00 +0800

    Lesson of Week 9 C

---
+++ week9c/mysql_insert.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "webprogss211";
+$password = "webprogss211";
+$dbname = "webprogss211";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO kavillanueva_myguests (name, email, website, comment, gender)
+VALUES ('omega', 'kavillanueva@example.com', 'http://localhost/lab2', 'Noice!', 'Male')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
