6ac7e334974893c5de838203ab37250f1405f081
Klarenz <kavillanueva@Studetn.apc.edu.ph>
2023-02-10 13:58:13 +0800

    Update

--- week9/validation_complete.php
+++ week9/validation_complete.php
@@ -98,7 +98,38 @@ echo "<br>";
 echo $comment;
 echo "<br>";
 echo $gender;
-?> 
+?>
+
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "webprogss211";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO kavillanueva_myguests (name, email, website, comment, gender)
+    VALUES ('$name', '$email', '$website', '$comment', '$gender')";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+
+}
+?>
 
 </body>
 </html>
